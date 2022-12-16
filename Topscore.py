111d031d3d98f88ec3fc3a41d48da0ccdd890652
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-12-16 08:58:32 +0100

    display score

---
+++ Topscore.py
@@ -0,0 +1,11 @@
+
+
+class Topscore:
+    def __init__(self):
+        self.high_score = 0
+        
+    def top_score(self, score):
+        if score > self.high_score:
+            self.high_score = score
+        
+        return self.high_score
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from Player import Player
 from Enemy import Enemy
 from Fire import Fire
+from Topscore import Topscore
 
 pygame.init()
 
@@ -21,6 +22,9 @@ CLOCK = pygame.time.Clock()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("Mario Clone")
+font = pygame.font.SysFont('forte', 20)
+
+topscore = Topscore()
 
 
 def game_loop():
@@ -32,6 +36,9 @@ def game_loop():
         player = Player()
         add_new_fire_counter = 0
         fires_list = []
+        global SCORE
+        SCORE = 0
+        global HIGH_SCORE
         while True:
             window.fill(BLACK)
             enemy.update(window)
@@ -45,6 +52,7 @@ def game_loop():
             for fire in fires_list:
                 if fire.fire_img_rect.left < 0:
                     fires_list.remove(fire)
+                    SCORE += 1
                 fire.update(window)                    
             print('o')
             
@@ -67,10 +75,39 @@ def game_loop():
                     elif event.key == pygame.K_DOWN:
                         player.down = True
                         player.up = False
+                        
+
+             
+            score_font = font.render("Score: " + str(SCORE), True, GREEN)
+            score_font_rect = score_font.get_rect()
+            score_font_rect.center = (200, score_font_rect.height/2)
+            window.blit(score_font, score_font_rect)
+            
                 
             player.update(window)
+            
+            for fire in fires_list:
+                if fire.fire_img_rect.colliderect(player.player_img_rect):
+                    game_over()
+            
             pygame.display.update()
             CLOCK.tick(FPS)
+
+
+
+def game_over():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                
+                game_loop()
+            pygame.display.update()
 
 
 def start_game():
